05f9960f555af8d0083af05741854f6a8078900c
sachin <sachinphanse31@gmail.com>
2025-08-02 17:50:02 +0530

    Initial commit with Task 1 and Task 2 scripts

---
+++ Assignmen_1.py
@@ -0,0 +1,31 @@
+# Task 1: Perform Basic Mathematical Operations
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+
+a = int(input("Enter first Number: "))
+b = int(input("Enter second Number: " ))
+
+# Perform Basic Mathematical Operations
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+
+# Task 2: Create a Personalized Greeting
+# Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello, " + full_name + "! Welcome to the python program")
+
+Create a GitHub repository and upload your Python scripts (.py files).
+Ensure the repository includes a README.md file that describes the functionality of your programs.
+Add both Task 1 and Task 2 scripts in the same repository.
+Submit the link to your GitHub repository once uploaded.
